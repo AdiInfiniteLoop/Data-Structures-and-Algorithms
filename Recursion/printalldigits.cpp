d50b85a4076479641c1dc7d2039e6d4446a5628e
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2023-09-07 11:36:59 +0530

    fourth commit

--- Recursion/printalldigits.cpp
+++ Recursion/printalldigits.cpp
@@ -2,11 +2,12 @@
 void printDigits(int n){
     if(n == 0)
         return ;
-    printDigits(n/10);
+    printDigits(n/10);//for printing reverese order ,, swap line 5 and line 6  
     std::cout<<n%10<<" is the digit"<<std::endl;
 
 }
 int main(){
-    int n = 132124;
+    int n = 0025;   //In C++, when started with leading zero it is considered as an octal value
+    if(n == 0) std::cout<<0; //Handling the edge case
     printDigits(n);
 }
