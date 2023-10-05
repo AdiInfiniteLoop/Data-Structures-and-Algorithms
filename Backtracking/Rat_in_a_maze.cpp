6d3a4b98549a2347fbca8ead2396a70b0ef37503
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2023-10-05 13:30:35 +0530

    Rat in a maze 3

--- Backtracking/Rat_in_a_maze.cpp
+++ Backtracking/Rat_in_a_maze.cpp
@@ -1,54 +1,93 @@
 #include <iostream>
-#include <vector>
-
+#include<vector>
 using namespace std;
 
-// A function to check if a given cell is valid.
-bool isSafe(int maze[4][4], int srcx, int srcy) {
-  return (srcx >= 0 && srcx < 4 && srcy >= 0 && srcy < 4 && maze[srcx][srcy] == 1);
+bool isSafe(int i, int j, int row, int col, int arr[][4],
+ vector<vector<bool> > &visited) {
+  if( ((i>=0 && i < row) && (j >= 0 && j< col)) &&
+      (arr[i][j] == 1 ) &&
+      (visited[i][j]==false)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-// A recursive function to find all possible pathways for the rat to reach the destination.
-void solvethemaze(int maze[4][4], int srcx, int srcy, vector<string> &pathways, string output) {
-  // If the rat has reached the destination, add the current path to the list of pathways.
-  if (srcx == 3 && srcy == 3) {
-    pathways.push_back(output);
+void solveMaze(int arr[][4], int row,int col, int i, int j,
+vector<vector<bool> > &visited, vector<string>& path, string output  ) {
+
+  //base case
+  if(i == row-1 && j == col-1) {
+    //answer found
+    path.push_back(output);
     return;
-  }
-
-  // Mark the current cell as visited.
-  maze[srcx][srcy] = 0;
-
-
-
-
-  // Recursivelsrcy explore all possible pathways from the current cell.
-  if (isSafe(maze, srcx + 1, srcy)) {
-    solvethemaze(maze, srcx + 1, srcy, pathways, output + " DOWN ");
-  }
-  if (isSafe(maze, srcx, srcy + 1)) {  
-    solvethemaze(maze, srcx, srcy + 1, pathways, output + " RIGHT");
-  }
-  if (isSafe(maze, srcx - 1, srcy)) {
-    solvethemaze(maze, srcx - 1, srcy, pathways, output + " UP");
-  }
-  if (isSafe(maze, srcx, srcy - 1)) {
-    solvethemaze(maze, srcx, srcy - 1, pathways, output + " LEFT");
   } 
 
-  // Unmark the current cell as visited.
-  maze[srcx][srcy] = 1;
+  //Down -> i+1, j
+  if(isSafe(i+1, j, row, col, arr, visited)) {
+    visited[i+1][j] = true;
+    solveMaze(arr, row, col, i+1, j, visited, path, output + 'D');
+    //backtrack 
+    visited[i+1][j] = false;
+  }
+
+  //Left -> i, j-1
+  if(isSafe(i, j-1, row, col, arr, visited)) {
+    visited[i][j-1] = true;
+    solveMaze(arr, row, col, i, j-1, visited, path, output + 'L');
+    //backtrack 
+    visited[i][j-1] = false;
+  }
+
+  //Right -> i, j+1
+  if(isSafe(i, j+1, row, col, arr, visited)) {
+    visited[i][j+1] = true;
+    solveMaze(arr, row, col, i, j+1, visited, path, output + 'R');
+    //backtrack 
+    visited[i][j+1] = false;
+  }
+
+  //Up-> i-1, j
+if(isSafe(i-1, j, row, col, arr, visited)) {
+    visited[i-1][j] = true;
+    solveMaze(arr, row, col, i-1, j, visited, path, output + 'U');
+    //backtrack 
+    visited[i-1][j] = false;
+  }
+
+
 }
 
 int main() {
-  int maze[4][4] = {{1, 0,0, 1}, {1, 1, 0,1}, {1,1, 1, 1},{0,0,1,1}};
+  int maze[4][4] = { {1,0,0,0},{1,1,0, 1},{1,1,0,0},{0,1,1,1}};
 
-  vector<string> pathways;
-  solvethemaze(maze, 0, 0, pathways, "");
-
-  // Print all possib<le pathways.
-  for (auto itr : pathways) {
-    cout << itr << endl;
+  if(maze[0][0] == 0) {
+    cout << "No Path Exists " << endl;
+    return 0;
   }
-  if(pathways.empty())std::cout<<"No ways are possible";
+
+  int row = 4;
+  int col = 4;
+
+  vector<vector<bool> > visited(row, vector<bool>(col,false));
+  //src ki value k true mark kredete h 
+  visited[0][0] = true;
+
+  vector<string> path;
+  string output = "";
+
+  solveMaze(maze,row,col, 0,0, visited, path, output);
+
+  cout << "printing the results " << endl;
+  for(auto i: path) {
+    cout << i << " ";
+  }
+  cout << endl;
+  if(path.empty()){
+    cout << "No Path Exists " << endl;
+  }
+
+  
+  return 0;
 }
