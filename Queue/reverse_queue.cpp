dcc037c901879438002dd2a2667cb6c0a47a2c98
Aditya <adiyouaresosmart@gmail.com>
2023-11-01 08:18:29 +0530

    Reverse using stack

---
+++ Queue/reverse_queue.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+
+using std:: cout;
+using std::endl;
+static void Print(std::queue<int> q){
+
+    while(!q.empty())
+    {
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+}
+int main(){
+    std::queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    cout<<"Before reversing ";
+    Print(q);
+    cout<<endl;
+    std::stack<int> st;
+    while(!q.empty()){
+        st.push(q.front());
+        q.pop();
+    }
+    cout<<"After reversing ";
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+}
