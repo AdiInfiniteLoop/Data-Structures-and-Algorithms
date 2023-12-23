25070b2f10ce96a01280bf643aee28a8bb31d0e4
Aditya <adiyouaresosmart@gmail.com>
2023-12-23 17:52:59 +0530

    Delete Node in BST

--- Binary_Search_Trees/deletion_in_bst.cpp
+++ Binary_Search_Trees/deletion_in_bst.cpp
@@ -1,22 +1,23 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 vector<int> ans;
-class Node{
+class TreeNode{
     public:
-    int data;
-    Node* left;
-    Node* right;
-    Node():data(0),left(nullptr),right(nullptr){}
-    Node(int data):data(data),left(nullptr),right(nullptr){}
-    ~Node(){
-        this->data = data;
+    int key;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode():key(0),left(nullptr),right(nullptr){}
+    TreeNode(int key):key(key),left(nullptr),right(nullptr){}
+    ~TreeNode(){
+        this->key = key;
         this->left = NULL;
         this->right = NULL;
     }
 };
-Node* maximum(Node* root){
-    Node* temp = root;
+TreeNode* maximum(TreeNode* root){
+    TreeNode* temp = root;
     if(!temp) return NULL;
     if(temp->right)
         return maximum(temp->right); //don't forget to return as when we make recursive calls but not returning and updating the values 
@@ -24,75 +25,89 @@ Node* maximum(Node* root){
 }
 
 
-void inordertraversal(Node* root,vector<int> &ans){
+void inordertraversal(TreeNode* root,vector<int> &ans){
     if(!root) return ;
     inordertraversal(root->left,ans);
-    ans.push_back(root->data);
+    ans.push_back(root->key);
     inordertraversal(root->right,ans);
     return ;
 }
 
-Node* insertinBst(Node* root, int data){
+TreeNode* insertinBst(TreeNode* root, int key){
     if(!root){
-        root = new Node(data);
+        root = new TreeNode(key);
         return root;
     }
 
-    if(root->data > data){
-        root->left = insertinBst(root->left,data);
+    if(root->key > key){
+        root->left = insertinBst(root->left,key);
     }
     else{
-        root->right = insertinBst(root->right,data);
+        root->right = insertinBst(root->right,key);
     }
     return root;
 }
 
-Node* takeinput(Node* root){
-    int data;
-    cin>>data;
-    while(data != -1){
-        root = insertinBst(root,data);
-        cin>>data;
+TreeNode* takeinput(TreeNode* root){
+    int key;
+    cin>>key;
+    while(key != -1){
+        root = insertinBst(root,key);
+        cin>>key;
     }
     return root;
 }
 
-Node* deletenodefromBst(Node* root, int data){
+vector<int> levelordertraversal(TreeNode* root){
+    if(!root) return ans;
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        TreeNode* temp = q.front();
+        ans.push_back(temp->key);
+        q.pop();
+        if(temp->left) q.push(temp->left);
+        if(temp->right) q.push(temp->right);
+    }
+    return ans;
+
+}
+TreeNode* deleteTreeNodefromBst(TreeNode* root, int key){
     //Base case
     if(!root) return NULL;
-    //Find the node we want to delete
-    if(root->data == data){
+    //Find the TreeNode we want to delete
+    if(root->key == key){
         //Mil gaya
         if(!root->left && !root->right){
             delete root;
             return NULL;
         }
         if(root->left && !root->right){
-            Node* children_of_leftsubtree = root->left;
+            TreeNode* children_of_leftsubtree = root->left;
             delete root;
             return children_of_leftsubtree;
         }
         if(!root->left && root->right){
-            Node* children_of_rightsubtree = root->right;
+            TreeNode* children_of_rightsubtree = root->right;
             delete root;
             return children_of_rightsubtree;
         }
         if(root->right && root->left){
-            Node* temp = root->left;
-            Node* max_value = maximum(root->left);
-            root->data = max_value->data;
+            TreeNode* temp = root->left;
+            TreeNode* max_value = maximum(root->left);
+            root->key = max_value->key;
             // delete max_value; <- NOT THIS!!!!!!;
-            root->left = deletenodefromBst(root->left,max_value->data);
+            root->left = deleteTreeNodefromBst(root->left,max_value->key);
             return root;
 
         }
 
     }
-    if(data < root->data){
-        root->left =  deletenodefromBst(root->left,data);
+    if(key < root->key){
+        root->left =  deleteTreeNodefromBst(root->left,key);
     }
-    if(data > root->data){
-        root->right =  deletenodefromBst(root->right,data);
+    if(key > root->key){
+        root->right =  deleteTreeNodefromBst(root->right,key);
     }
     return root;
 }
@@ -100,8 +115,8 @@ Node* deletenodefromBst(Node* root, int data){
 // 10 20 5 11 17 2 4 8 6 25 15 -1
 int main(){
 
-    Node* root = NULL;
-    cout<<"Enter the data for the tree\n";
+    TreeNode* root = NULL;
+    cout<<"Enter the key for the tree\n";
     root =takeinput(root);
 
     cout<<"The inorder traversal is: \n";
@@ -116,11 +131,18 @@ int main(){
 
     cout<<"Enter the value of key\n";
     cin>>key;
-    root = deletenodefromBst(root,key);
+    root = deleteTreeNodefromBst(root,key);
 
     inordertraversal(root,ans);
-        for(int it: ans){
+    for(int it: ans){
         cout<< it<<" ";
+        ans.clear();
     }
+    levelordertraversal(root);
+    for(int it: ans){
+        cout<< it<<" ";
+        ans.clear();
+    }
+    
 }   
 
