1cf53a3332854d94556ab741255aa437cc4c4397
Aditya <adiyouaresosmart@gmail.com>
2024-01-18 19:44:20 +0530

    Heap Sort

--- Heap/heapify.cpp
+++ Heap/heapify.cpp
@@ -2,29 +2,41 @@
 using namespace std;
 
 void heapify(int *arr, int n, int i) {
-    int index = i;
-    int largest = index;
-    int left = 2 * index;
-    int right = 2 * index + 1;
+    int largest = i;
+    while(true){
+        int left = 2*i;
+        int right = 2*i+1;
+        if(arr[left] > arr[i] && left < n)largest = left;
+        if(arr[right] > arr[i] && right < n)largest = right;
 
-    if (left <= n && arr[index] < arr[left]) largest = left; //left <= n is imp/\.
-    if (right <= n && arr[largest] < arr[right]) largest = right;
-
-    if (index != largest) {
-        std::swap(arr[index], arr[largest]);
-        heapify(arr, n, largest);
-    } 
+        if(i != largest){
+            swap(arr[i],arr[largest]);
+            i = largest;
+        } else{
+            break;
+        }
+    }
 }
 
 void buildHeap(int arr[], int n) {
-    for (int i = n / 2; i >= 0; --i) {  // Start from 0 instead of 1
+    for (int i = n / 2; i >= 1; --i) {  // Start from 0 instead of 1
         heapify(arr, n, i);
     }
 }
 
+
+void heapSort(int * arr, int n){
+    
+    while(n != 1){
+        swap(arr[1],arr[n--]);
+        heapify(arr,n,1);
+    }
+    
+}
+
 int main() {
-    int *arr = new int[13] {-1, 76, 45, 3, 4, 86, 42, 35, 87, 98 ,43 ,23 ,12};  // Allocate memory for 13 elements
-    int n = 10;
+    int *arr = new int[13] {-1,5,2,3,1};  // Allocate memory for 13 elements
+    int n = 4;
 
     buildHeap(arr, n);
 
@@ -32,8 +44,17 @@ int main() {
         cout << " " << arr[i] << " ";
     }
 
-    // Don't forget to free the allocated memory
-    delete[] arr;
+    heapSort(arr,n);
+    cout<<"\n";
+    for (int i = 1; i <= n; ++i) {
+        cout << " " << arr[i] << " ";
+    }
+
 
     return 0;
 }
+
+
+
+
+//do the same code for 0based Indexing
